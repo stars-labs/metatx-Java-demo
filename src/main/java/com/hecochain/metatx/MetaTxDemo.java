53c11121bdfa4e1b6693dfafa16f5d042ec44c16
hechangbin <hechangbin@huobi.com>
2021-02-18 18:57:16 +0800

    add metatx docs

--- src/main/java/com/hecochain/metatx/MetaTxDemo.java
+++ src/main/java/com/hecochain/metatx/MetaTxDemo.java
@@ -1,10 +1,12 @@
 package com.hecochain.metatx;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hecochain.metatx.signer.MetaData;
 import com.hecochain.metatx.signer.MetaTxService;
 import com.hecochain.metatx.signer.Util;
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.util.encoders.Hex;
+import org.ethereum.util.ByteUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
@@ -22,7 +24,7 @@ public class MetaTxDemo {
     @Autowired
     private ObjectMapper objectMapper;
 
-    public void getMetaSendResult(String tx) throws Exception {
+    public void encode(String tx) throws Exception {
         //your rpc
         Web3j web3j = Web3j.build(new HttpService("https://http-mainnet-node.huobichain.com"));
 
@@ -45,6 +47,15 @@ public class MetaTxDemo {
         //send this to the network or response to user
 
         //  EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(metaRawTx).send();
+    }
+
+    public void decode() throws Exception {
+        Transaction tx = new Transaction(Hex.decode("f8d0820113843b9aca0082c3509488b7790f47e8875c0f8cf63e507c550586be55d688016345785d8a0000b860234d6574615472616e73616374696f6e23f84d831c96648207d0820124a0d98bcbc63448eebfe290d287cd63dcda129e0ae10c87aa10777909eeb336ac39a022510088aef0f7221f9a95575efe9476baf4e66560920891c8dca091aca0cb6e80820123a04d7f8d6a891b2b88297e97f6a09cb19e6713c49fc3f3bd7bf14a981833c03729a01250b7dfa70c9d74c9d010e6aeb03de1e8616b3074d297b2a08a15238e72114e"));
+        String rawData = Hex.toHexString(tx.getData());
+        System.out.println(rawData);
+        MetaData metaData = new MetaData(ByteUtil.hexStringToBytes(rawData.substring("234d6574615472616e73616374696f6e23".length())));
+        metaData.rlpPares();
+        System.out.println("hh");
     }
 
 }
